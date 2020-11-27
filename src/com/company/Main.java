a307a575998cdc66e0a3a09effd58d24dc5e5541
PAWEŁ <pawel.mlo@op.pl>
2020-11-27 14:55:17 +0100

    Wypisanie talbicy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,20 @@ public class Main {
         int tmp = tab[index1];
         tab[index1] = tab[index2];
         tab[index2] = tmp;
-        // return tab;
+
+    }
+    static void wypisz(int []tab)
+    {
+        for (int x: tab) {
+            System.out.println(x);
+        }
     }
 
     public static void main(String[] args) {
 	int []tab = {1,2,3,4,12,54,-5};
 
 	zamiana(tab,0,2);
-            System.out.println(tab[0]);
+	wypisz(tab);
 
     }
 }
