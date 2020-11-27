f33aac5e57c70593252b62a0599d48040224440a
PAWEŁ <pawel.mlo@op.pl>
2020-11-27 14:51:45 +0100

    Zamiana indexów

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,19 @@ package com.company;
 
 public class Main {
 
+    static void zamiana(int []tab, int index1, int index2)
+    {
+        int tmp = tab[index1];
+        tab[index1] = tab[index2];
+        tab[index2] = tmp;
+        // return tab;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	int []tab = {1,2,3,4,12,54,-5};
+
+	zamiana(tab,0,2);
+            System.out.println(tab[0]);
+
     }
 }
